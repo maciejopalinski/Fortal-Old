03069e4bc16a494faa287b521fae9a125021d6e3
Maciej Opaliński <poprostumieciek@gmail.com>
2021-08-06 18:33:18 +0200

    Add empty GTest .cpp file
    For future use!

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+#define GTEST_DECLARES_MAIN
+
+#include <gtest/gtest.h>
+using namespace std;
+using namespace testing;
+
+TEST(test_suite_name, test_name)
+{
+    ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[])
+{
+    InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
