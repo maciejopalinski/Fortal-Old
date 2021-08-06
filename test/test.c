1c8b0c87af48e711890ec244e8fdd5f95cf0f7a0
Maciej Opaliński <poprostumieciek@gmail.com>
2021-08-06 18:33:40 +0200

    Add example `test.c` file to test Lexer class

---
+++ test/test.c
@@ -0,0 +1,28 @@
+/*
+    this is a multiline comment
+
+
+    block comment
+*/
+
+/* multiline */
+
+//  this is a single line comment
+//    this is a single line comment
+//      this is a single line comment
+//        this is a single line comment
+//          this is a single line comment
+
+double = int + double;
+
+int main()
+{
+    int x = 0;
+    if (x + 2 == 3) return 1;
+    else return 0;
+}
+
+1.000003;
+"test";
+'c';
+1e3
