ee88fe90e547e89b9755f1fff218f83d8e1ed3a7
Maciej Opaliński <poprostumieciek@gmail.com>
2021-08-10 22:16:02 +0200

    Update tests

--- test/test.f
+++ test/test.f
@@ -13,16 +13,31 @@
 //        this is a single line comment
 //          this is a single line comment
 
-double = int + double;
+void test(int a, int b)
+{
+
+}
 
 int main()
 {
-    int x = 0;
+    int y;
+    int x = 0 + 0;
     if (x + 2 == 3) return 1;
     else return 0;
-}
 
-1.000003;
-"test";
-'c';
-1e3
+    test(x, y);
+
+    -1.000003
+    3
+    -1
+    0
+    5.5
+    5.163813681435419142
+    1e-2
+    1e20
+    "test"
+    'ct'
+    x &= (false | true)
+    nullptr
+}
+int
