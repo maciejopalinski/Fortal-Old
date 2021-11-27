3764c7fbf8f8a6405e1df66e0ae0925cf5d7f7d8
Maciej Opaliński <poprostumieciek@gmail.com>
2021-11-27 15:22:37 +0100

    Add `TokenHasType` class

---
+++ src/Token/TokenHasType.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "TokenBase.hpp"
+
+template<class T>
+class TokenHasType : public TokenBase
+{
+    private:
+        T type;
+
+    protected:
+        static vector<string> lexer_matches;
+
+    public:
+        TokenHasType(T type)
+        {
+            this->type = type;
+        }
+
+        T getType()
+        {
+            return this->type;
+        }
+
+        void setType(T type)
+        {
+            this->type = type;
+        }
+
+        virtual string getTypeString(T type) = 0;
+
+        static vector<string> getLexerMatches();
+        static T getLexerTypeFromMatch(string match);
+        static bool getLexerWhitespaceAfter();
+};
