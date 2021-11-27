7560eef98af7d63320c6ac3e8cbdcf6d7daa4ec8
Maciej Opaliński <poprostumieciek@gmail.com>
2021-11-27 15:22:15 +0100

    Add `TokenBase` class

---
+++ src/Token/TokenBase.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "../Location/Location.hpp"
+#include "TokenType.hpp"
+
+class TokenBase
+{
+    public:
+        Location location;
+
+        virtual TokenType getTokenType() = 0;
+        virtual const char *getTokenTypeString() = 0;
+
+        virtual string getDebug() = 0;
+};
