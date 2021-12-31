d693ab69f8ec34695db92ac8fa0487af7547596f
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-31 18:39:52 +0100

    test: Remove old tests and add new ones

---
+++ test/fortal/object.f
@@ -0,0 +1,20 @@
+// this is a comment before package defintion
+
+package fortal.examples.object_oriented;
+
+import fortal.sth.sth2.test;
+import fortal.sth2.*;
+
+class Animal;
+
+class Dog;
+
+class Cat;
+
+// this is a comment
+
+public static entrypoint void main(String[] args) {
+
+}
+
+public static final int32 EXIT_CODE;
---
+++ test/fortal/test.f
@@ -0,0 +1,49 @@
+/* fortal::ignore_parser */
+
+package fortal.examples.lexer;
+
+/*
+    this is a multiline comment
+
+    also known as a
+    block comment
+*/
+
+import fortal.test.import_test;
+
+/* multiline */
+
+//  this is a single line comment
+//      this is a single line comment
+//          this is a single line comment
+
+void test(int32 a, int32 b)
+{
+    a += b;
+}
+
+int32 main()
+{
+    int32 y;
+    if (y != y) return y;
+    int32 x = 0 + 0;
+    if (x + 2 == 3) return 1;
+    else return 0;
+
+    test(x, y);
+
+    -1.000003
+        3
+        - 1
+        0
+        5.5
+        5.163813681435419142
+        1e-2
+        1e20
+        "test"
+        't'
+        'cttttt'
+        x &= (false | true)
+             return null;
+}
+int32
