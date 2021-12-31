81d96ccb4161e17e5b6ae761a7c841cb1b5b6b80
Maciej Opaliński <poprostumieciek@gmail.com>
2021-12-31 18:41:07 +0100

    refactor: Divide `ErrorHandler` into separate files

---
+++ src/ErrorHandler/ErrorHandler.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <stdio.h>
+
+#include "../Location/Location.h"
+#include "ErrorLevel.h"
+
+class ErrorHandler
+{
+    public:
+        ErrorLevel reporting_level;
+
+        ErrorHandler(ReportingLevel level = E_DEFAULT);
+
+        void setReportingLevel(ReportingLevel level = E_DEFAULT);
+
+        const char *getPrefix(ReportingLevel flag);
+
+        FILE *getStream(ReportingLevel flag);
+
+        template<class... Args>
+        void log(ReportingLevel level, const char *f1, Args... args);
+
+        template<class... Args>
+        void logLocation(ReportingLevel level, Location location, const char *f1, Args... args);
+
+        void terminate_compilation(int exit_code = 1);
+
+        void throw_no_input_files();
+
+        void log_no_such_file_or_directory(const char *filename);
+
+        void throw_invalid_token(Location location, const char c);
+
+        void throw_unexpected_token(Location location, string custom_message);
+        void throw_unexpected_token(Location location, string unexpected, string expected);
+
+        void log_empty_character_literal(Location location);
+};
+
+template<class... Args>
+void ErrorHandler::log(ReportingLevel level, const char *f1, Args... args)
+{
+    if (reporting_level.testFlag(level))
+    {
+        auto stream = getStream(level);
+        auto prefix = getPrefix(level);
+        string f2   = format("%s %s\n", prefix, f1);
+
+        fprintf(stream, f2.c_str(), args...);
+    }
+}
+
+template<class... Args>
+void ErrorHandler::logLocation(ReportingLevel level, Location location, const char *f1, Args... args)
+{
+    string location_str = location.getPrefix();
+    string f2           = format("%s: %s", location_str.c_str(), f1);
+
+    log(level, f2.c_str(), args...);
+}
