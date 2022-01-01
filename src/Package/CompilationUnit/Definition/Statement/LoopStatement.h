d6ad8d3234ce0e3bef4d4c59dba1afcb91679e33
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-01 23:28:02 +0100

    feat: Add `LoopStatement` class

---
+++ src/Package/CompilationUnit/Definition/Statement/LoopStatement.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "StatementHasKind.h"
+#include "StatementHasCondition.h"
+#include "StatementHasBody.h"
+
+enum StatementLoopType
+{
+    STATEMENT_LOOP_FOR,
+    STATEMENT_LOOP_WHILE,
+    STATEMENT_LOOP_DO_WHILE
+};
+
+extern const char *StatementLoopType_printable_strings[];
+
+class LoopStatement : public StatementHasKind<StatementLoopType>, public StatementHasBody, public StatementHasCondition
+{
+    private:
+        shared_ptr<Expression> for_init;
+        shared_ptr<Expression> for_iter;
+
+    public:
+        LoopStatement(StatementLoopType kind) : StatementHasKind(kind) {}
+
+        StatementType getType()
+        {
+            return STATEMENT_LOOP;
+        }
+
+        static string getPrintableKindString(StatementLoopType kind);
+        string getPrintableKindString();
+
+        shared_ptr<Expression> getForInit();
+        void setForInit(shared_ptr<Expression> for_init);
+
+        shared_ptr<Expression> getForIter();
+        void setForIter(shared_ptr<Expression> for_iter);
+};
