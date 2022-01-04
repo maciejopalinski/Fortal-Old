803c7c0c25eeed4ccc32f8f0ba71bf1fc88e89fb
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-04 14:03:20 +0100

    feat: Add `Expression` base class

---
+++ src/Package/CompilationUnit/Definition/Expression/Expression.cpp
@@ -0,0 +1,65 @@
+#include "Expression.h"
+
+const char *ExpressionType_printable_strings[] =
+{
+    "Literal",
+    "VariableDefinition",
+    "NewStatement",
+    "Identifier",
+    "Paren",
+    "Operation"
+};
+
+Expression::~Expression()
+{
+}
+
+const char *Expression::getTypeString(ExpressionType type)
+{
+    auto name = magic_enum::enum_name<ExpressionType>(type);
+    return name.data();
+}
+
+const char *Expression::getTypeString()
+{
+    return getTypeString(getType());
+}
+
+const char *Expression::getPrintableTypeString(ExpressionType type)
+{
+    auto idx = magic_enum::enum_index<ExpressionType>(type);
+    if (idx.has_value())
+    {
+        return ExpressionType_printable_strings[idx.value()];
+    }
+    return "(null)";
+}
+
+const char *Expression::getPrintableTypeString()
+{
+    return getPrintableTypeString(getType());
+}
+
+string Expression::getDebug(string indent, bool last, string custom)
+{
+    string output = indent;
+
+    if (last)
+    {
+        output += "└── ";
+        indent += "    ";
+    }
+    else
+    {
+        output += "├── ";
+        indent += "│   ";
+    }
+
+    output +=
+        format(
+            "[Expression] [%s] ",
+            getPrintableTypeString()
+        ) + custom + "\n";
+
+    return output;
+}
--- src/Package/CompilationUnit/Definition/Expression/Expression.h
+++ src/Package/CompilationUnit/Definition/Expression/Expression.h
@@ -1,3 +1,37 @@
 #pragma once
+#include <string>
+#include <memory>
+using std::string;
+using std::shared_ptr;
+using std::make_shared;
 
-class Expression;
+#include "../../../../Utils/MagicEnum.hpp"
+#include "../../../../Utils/Format.h"
+
+enum ExpressionType
+{
+    EXPRESSION_LITERAL, // <Literal>
+    EXPRESSION_VARIABLE_DEFINITION, // <VariableDefinition>
+    EXPRESSION_NEW_STATEMENT, // new <Expression>
+    EXPRESSION_IDENTIFIER, // <Identifier>
+    EXPRESSION_PAREN, // ( <Expression> )
+    EXPRESSION_OPERATION
+};
+
+extern const char *ExpressionType_printable_strings[];
+
+class Expression
+{
+    public:
+        virtual ~Expression();
+
+        virtual ExpressionType getType() = 0;
+
+        static const char *getTypeString(ExpressionType type);
+        const char *getTypeString();
+
+        static const char *getPrintableTypeString(ExpressionType type);
+        const char *getPrintableTypeString();
+
+        virtual string getDebug(string indent, bool last = true, string custom = "");
+};
