eb97d81da23b1e13cf8f2b458ffd1ad9445b15dc
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-04 21:51:31 +0100

    test: Update tests
    * expressions
    * operator precedence
    * alias definition

--- test/fortal/object.f
+++ test/fortal/object.f
@@ -5,10 +5,20 @@ package fortal.examples.object_oriented;
 // import fortal.sth.sth2.test;
 // import fortal.sth2.*;
 
+alias bool = int1;
+alias char = int8;
+alias byte = uint8;
+alias short = int16;
+alias ushort = uint16;
+alias int = int32;
+alias uint = uint32;
+alias long = int64;
+alias ulong = uint64;
+
 public class Animal {
     void eat() {
-        int32 i = 10; // this works
-        // int i; // TODO: fix this
+        void i = 10;
+        CustomType i;
     }
     void eat2() {}
 
@@ -46,6 +56,8 @@ public static entrypoint void main2(String[] args = test, int32 test) throws Exc
 
     2e5()[10]();
 
+    i++;
+
     buffer[i++] = ' ';
 
     if ((a + b) > ((a > b) ? a : b))
@@ -60,7 +72,21 @@ public static entrypoint void main2(String[] args = test, int32 test) throws Exc
 
     return t ? ++i : i;
 
-    return a + b * c % d;
+    return a * b + c * d;
+
+    return a + b * c * d * e;
+
+    for (size_t i = 0; i < 100; i++)
+    {
+        printf(i);
+    }
+
+    if (i == 1 && test);
+
+    do {
+        
+    }
+    while(i == 1);
 }
 
 public static final int32 EXIT_CODE = 1;
