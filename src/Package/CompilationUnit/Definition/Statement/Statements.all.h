3fe8daa7b84d1f220cffbd144b30a2e339198f25
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-04 15:47:17 +0100

    feat: Add `Statements.all.h` to simplify #including

---
+++ src/Package/CompilationUnit/Definition/Statement/Statements.all.h
@@ -0,0 +1,8 @@
+#include "ExpressionStatement.h"
+#include "BlockStatement.h"
+#include "IfStatement.h"
+#include "LoopStatement.h"
+#include "FlowControlStatement.h"
+#include "TryStatement.h"
+#include "CatchStatement.h"
+#include "EmptyStatement.h"
