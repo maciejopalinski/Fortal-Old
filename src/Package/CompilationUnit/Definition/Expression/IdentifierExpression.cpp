d46c2192bed5fee97d95b80acb6d7380a3d97daa
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-04 14:04:57 +0100

    feat: Add `IdentifierExpression` class

---
+++ src/Package/CompilationUnit/Definition/Expression/IdentifierExpression.cpp
@@ -0,0 +1,23 @@
+#include "IdentifierExpression.h"
+
+IdentifierExpression::IdentifierExpression(shared_ptr<TokenIdentifier> identifier)
+{
+    setIdentifier(identifier);
+}
+
+shared_ptr<TokenIdentifier> IdentifierExpression::getIdentifier()
+{
+    return identifier;
+}
+
+void IdentifierExpression::setIdentifier(shared_ptr<TokenIdentifier> identifier)
+{
+    this->identifier = identifier;
+}
+
+string IdentifierExpression::getDebug(string indent, bool last, string custom)
+{
+    string output = Expression::getDebug(indent, last, identifier->getName() + custom);
+
+    return output;
+}
---
+++ src/Package/CompilationUnit/Definition/Expression/IdentifierExpression.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Expression.h"
+#include "../../../../Token/TokenIdentifier.h"
+
+class IdentifierExpression : public Expression
+{
+    private:
+        shared_ptr<TokenIdentifier> identifier;
+
+    public:
+        IdentifierExpression(shared_ptr<TokenIdentifier> identifier);
+
+        ExpressionType getType()
+        {
+            return EXPRESSION_IDENTIFIER;
+        }
+
+        shared_ptr<TokenIdentifier> getIdentifier();
+        void setIdentifier(shared_ptr<TokenIdentifier> identifier);
+
+        string getDebug(string indent, bool last = true, string custom = "");
+};
